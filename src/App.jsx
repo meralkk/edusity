e4fe641a8fcb20fb5b5750604eb53d712f0fb181
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-05 19:28:03 +0100

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import About from './components/About/About'
+import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <div className="container">
+      <Title subTitle ='Our PROGRAM' title='What We Offer'/>
+      <Programs/>
+      <About/>
+      </div>
     </div>
   )
 }
