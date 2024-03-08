87d3dbf5c1856d4278fe3dd794b04a13c316a758
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-08 18:05:08 +0100

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 
   return (
     <nav className= { `container ${sticky? 'dark-nav' : ''}`}>
-        <img src={logo} alt="" className='logo' />
+       <Link to="hero" ><img src={logo} alt="" className='logo' /></Link> 
         <ul className={mobileMenu?''  :'hide-mobile-menu'}>
             <li> <Link to='hero' smooth= { true } offset = { 0 } duration = { 500 }>Home</Link></li>
             <li> <Link to='programs' smooth= { true } offset = { -260 } duration = { 500 }>Program</Link></li>
